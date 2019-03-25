c12b1365eb9553a3414c0417ea994151a9395e71
Numperposition <xhxxbob@163.com>
2019-03-26 00:53:47 +1100

    second update of MelbGrid class functions

--- MelbGrid.py
+++ MelbGrid.py
@@ -1,4 +1,7 @@
 class MelbGrid(object):
+    postCount = 0         # record the post count for each grid.
+    hashtagsList = dict() # store the hashtags that belong to particular grid.
+
     def __init__(self, id, xmin, xmax, ymin, ymax):
         self.id = id
         self.xmin = xmin
@@ -6,7 +9,25 @@ class MelbGrid(object):
         self.ymin = ymin
         self.ymax = ymax
 
-    def checkInGrid(self, x, y):
+    def updateHashtagsList(self, text):
+        if text in self.hashtagsList.keys():
+            self.hashtagsList[text] += 1
+        else:
+            newDict = {text: 1}
+            self.hashtagsList.update(newDict)
+        return self.hashtagsList
+
+    def checkInGrid(self, x, y): # wait for discussion
         if x >= self.xmin and x <= self.xmax and y >= self.ymin and y <= self.ymax:
-            return True
-        return False
+            self.postCount += 1
+
+if __name__ == "__main__": # use for testing
+    testObj = MelbGrid("A1",0,0,0,0)
+    for i in range(0, 1):
+        MelbGrid.updateHashtagsList(testObj, "abc")
+
+    MelbGrid.updateHashtagsList(testObj, "def")
+    tagList = MelbGrid.updateHashtagsList(testObj, "def")
+    print(tagList)
+    # sort the hashtags from largest to lower for each grid
+    print(sorted(tagList.items(), key=lambda x: x[1], reverse=True))
