c1c9ec9838b3f52d68a655c82b3f7864e74f0b39
Numperposition <xhxxbob@163.com>
2019-03-26 22:49:28 +1100

    judge coordinates function added

--- MelbGrid.py
+++ MelbGrid.py
@@ -1,6 +1,7 @@
+import json
 class MelbGrid(object):
     postCount = 0         # record the post count for each grid.
-    hashtagsList = dict() # store the hashtags that belong to particular grid.
+    hashtagsList = None # store the hashtags that belong to particular grid.
 
     def __init__(self, id, xmin, xmax, ymin, ymax):
         self.id = id
@@ -8,26 +9,75 @@ class MelbGrid(object):
         self.xmax = xmax
         self.ymin = ymin
         self.ymax = ymax
+        self.hashtagsList = dict()  # initialize object dictionary
 
-    def updateHashtagsList(self, text):
-        if text in self.hashtagsList.keys():
-            self.hashtagsList[text] += 1
+    def updateHashtagsList(self, str):
+        if str in self.hashtagsList.keys():
+            self.hashtagsList[str] += 1
         else:
-            newDict = {text: 1}
+            newDict = {str: 1}
             self.hashtagsList.update(newDict)
-        return self.hashtagsList
 
-    def checkInGrid(self, x, y): # wait for discussion
-        if x >= self.xmin and x <= self.xmax and y >= self.ymin and y <= self.ymax:
-            self.postCount += 1
+    def checkInGrid(self, x, y, text): # each data line calls it by entering its x,y coordinates,and hashtags' text
+                                       # the outcome is to update each grid object's postCount and hashtagsList
+        if self.id == "A1":
+            if x >= self.xmin and x <= self.xmax and y >= self.ymin and y <= self.ymax:
+                self.postCount += 1
+                if text != None:
+                    self.updateHashtagsList(text)
+                return True
+        elif self.id == "A2" or self.id == "A3" or self.id == "A4" or self.id == "C5":
+            if x > self.xmin and x <= self.xmax and y >= self.ymin and y <= self.ymax:
+                self.postCount += 1
+                if text != None:
+                    self.updateHashtagsList(text)
+                return True
+        elif self.id == "B1" or self.id == "C1" or self.id == "D3":
+            if x >= self.xmin and x <= self.xmax and y > self.ymin and y <= self.ymax:
+                self.postCount += 1
+                if text != None:
+                    self.updateHashtagsList(text)
+                return True
+        else:    #"B2" "B3"  "B4" "C2" "C3"  "C4"  "D4"  "D5"
+            if x > self.xmin and x <= self.xmax and y > self.ymin and y <= self.ymax:
+                self.postCount += 1
+                if text != None:
+                    self.updateHashtagsList(text)
+                return True
+        return False
+
+def readMelbGrid(filename): # this function is used to load the MelbGrid objects from the file. return @list
+                            # It's not necessary exist in this file
+    file = open(filename, encoding='utf-8')
+    gridJson = json.load(file)
+    features = []
+    features = gridJson['features']
+    gridObjList = []
+    for feature in features:
+        id = feature['properties']['id']
+        xmin = feature['properties']['xmin']
+        xmax = feature['properties']['xmax']
+        ymin = feature['properties']['ymin']
+        ymax = feature['properties']['ymax']
+        melbGrid = MelbGrid(id, xmin, xmax, ymin, ymax)
+        gridObjList.append(melbGrid)
+        # print(type(gridObjList[0].xmin))   xmin is float, id is str
+    return gridObjList
+
 
 if __name__ == "__main__": # use for testing
-    testObj = MelbGrid("A1",0,0,0,0)
-    for i in range(0, 1):
-        MelbGrid.updateHashtagsList(testObj, "abc")
+    gridList = readMelbGrid("melbGrid.json")
+    for i in range(0, len(gridList)):
+        if gridList[i].checkInGrid(145.08645638, -37.96924206, "abcd"): #(x,y,hashtags_text) if find one grid,then stop matching
+            break
 
-    MelbGrid.updateHashtagsList(testObj, "def")
-    tagList = MelbGrid.updateHashtagsList(testObj, "def")
-    print(tagList)
+    for obj in gridList:
+        print(obj.id)
+        print(obj.postCount)
+        print(obj.hashtagsList)
+
     # sort the hashtags from largest to lower for each grid
-    print(sorted(tagList.items(), key=lambda x: x[1], reverse=True))
+    #print(sorted(tagList.items(), key=lambda x: x[1], reverse=True))
+
+
+
